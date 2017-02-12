aa3d00821634bc9cf7c7db31a8522b381610ed58
StephaneC <Stephane.castrec@gmail.com>
2017-02-12 22:02:22 +0100

    receive json.

--- src/main/Handlers.go
+++ src/main/Handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"net/http"
 	"encoding/json"
+	"log"
+	"time"
+	"strconv"
 )
 
 /**
@@ -14,7 +16,47 @@ func HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func ApiAiHookHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-	json.NewEncoder(w).Encode(id)
+	//first check that we have an input
+	if r.Body == nil {
+		http.Error(w, "Please send a request body", 400)
+		return
+	}
+	var error = "I don't understand what you said!"
+	//get request
+	var request ApiAiRequest
+	var response ApiAiResponse
+	request, err := decodeRequest(r)
+	if err != nil {
+		log.Println("Couldn't parse body for ApiAiHookHandler")
+		response = ApiAiResponse{DisplayText: error}
+		return;
+	} else {
+		log.Println("request.result.action "+response.DisplayText)
+		switch (request.Result.Action){
+		case "action.time":
+			response = ApiAiResponse{DisplayText:strconv.Itoa(time.Now().Hour())}
+			break;
+		/** add your actions here */
+		default:
+			response = ApiAiResponse{DisplayText: error}
+			break;
+
+		}
+	}
+	log.Println("response.displaytest "+response.DisplayText)
+	json.NewEncoder(w).Encode(response)
+}
+
+func decodeRequest(r *http.Request)(ApiAiRequest, error) {
+	var t ApiAiRequest
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&t)
+	if err != nil {
+		log.Println("Error parsing apiairequest", err)
+		panic(err)
+		return t, err
+	}
+	defer r.Body.Close()
+	return t, err
 }
